bd774168f511b8970041f4064db4e9dc2fa672cc
english-ra <englishra.dev@icloud.com>
2023-09-24 19:02:02 +0100

    Added animated burger for mobile

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,8 +1,14 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { useMediaQuery } from "../../util/useMediaQuery";
+
+
 
 export default function Nav() {
     const [toggled, setToggled] = useState(false);
+    const matches = useMediaQuery("(min-width: 1280px)");
+
+    console.log(matches);
 
     return (
         <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
@@ -36,11 +42,56 @@ export default function Nav() {
                 <a href="/">Hua.</a>
             </h1>
 
-            <div className="space-y-1">
-                <span className="block h-0.5 w-8 bg-black"></span>
-                <span className="block h-0.5 w-6 bg-black"></span>
-                <span className="block h-0.5 w-4 bg-black"></span>
-            </div>
+            {/* Check if we are on mobile or not */}
+
+            {matches && (
+                <div className="flex gap-12">
+                    <a href="/">Home</a>
+                    <a href="/services">Services</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            )}
+
+            {!matches && (
+                <div
+                    onClick={() => setToggled((prevToggle) => !prevToggle)}
+                    className="space-y-1.5 cursor-pointer z-50"
+                >
+                    <motion.span
+                        animate={{
+                            rotateZ: toggled ? 45 : 0,
+                            y: toggled ? 8 : 0,
+                        }}
+                        className="block h-0.5 w-8 bg-black"
+                    ></motion.span>
+                    <motion.span
+                        animate={{ width: toggled ? 0 : 24 }}
+                        className="block h-0.5 w-6 bg-black"
+                    ></motion.span>
+                    <motion.span
+                        animate={{
+                            rotateZ: toggled ? -45 : 0,
+                            y: toggled ? -8 : 0,
+                            width: toggled ? 32 : 16,
+                        }}
+                        className="block h-0.5 w-4 bg-black"
+                    ></motion.span>
+                </div>
+            )}
+
+            {toggled && !matches && (
+                <motion.div
+                    animate={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 25 }}
+                    className="fixed flex bg-white bottom-0 left-0 w-full h-screen items-center justify-center"
+                >
+                    <div className="flex flex-col gap-24 text-lg">
+                        <a href="/">Home</a>
+                        <a href="/services">Services</a>
+                        <a href="/contact">Contact</a>
+                    </div>
+                </motion.div>
+            )}
         </nav>
     );
 }
